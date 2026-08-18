4dafbba6b1d881141a7a03abd9ae663908063a49
Majestic44 <majestic44@users.noreply.github.com>
2026-08-18 01:35:05 -0400

    Remove unused WorkHistory component

--- client/src/templates/resume/ModernResume.jsx
+++ client/src/templates/resume/ModernResume.jsx
@@ -163,29 +163,6 @@ export function ModernResume({ resume, qrCodeUrl = '', onQrCodeReady }) {
   );
 }
 
-function WorkHistory({ work }) {
-  return (
-    <div className="resume-template-modern__timeline">
-      {work.map((item, index) => (
-        <article className={`resume-template-modern__timeline-item${isCurrentRole(item.dateLabel) ? ' is-current' : ''}`} key={`${item.company}-${item.position}-${index}`}>
-          <div className="resume-template-modern__role-row">
-            <div>
-              <h3>{item.position}</h3>
-              <p>{[item.company, item.location].filter(Boolean).join(' | ')}</p>
-            </div>
-            <span>{item.dateLabel}</span>
-          </div>
-          {item.highlights?.length ? (
-            <ul>
-              {item.highlights.map(highlight => <li key={highlight}>{highlight}</li>)}
-            </ul>
-          ) : null}
-        </article>
-      ))}
-    </div>
-  );
-}
-
 function getInitials(name) {
   return String(name || '')
     .split(' ')
